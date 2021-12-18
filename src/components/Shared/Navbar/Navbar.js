50a7221c78575084dc1bd5b716a048b47857a6b2
hanif365 <m.a.hanifkhan64@gmail.com>
2021-12-19 00:01:26 +0600

    Show Notification of Logged In user details

--- src/components/Shared/Navbar/Navbar.js
+++ src/components/Shared/Navbar/Navbar.js
@@ -5,7 +5,7 @@ import brandicon from '../../../Assets/Images/brandicon.png'
 import { UserContext } from '../../../App';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faOilCan, faUserMd } from '@fortawesome/free-solid-svg-icons';
+import { faAlignRight, faOilCan, faUserMd } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
     const [loggedInUser, setLoggedInUser] = useContext(UserContext);
@@ -32,7 +32,7 @@ const Navbar = () => {
             .then(res => res.json())
             .then(data => setIsSuperAdmin(data));
     }, [])
-    
+
     const showNotification = () => {
         const Swal = require('sweetalert2')
         Swal.fire({
@@ -42,6 +42,24 @@ const Navbar = () => {
             showConfirmButton: false,
             timer: 2500,
             timerProgressBar: true,
+        })
+    }
+
+    const showNotificationForIdentification = () => {
+        console.log(loggedInUser)
+        const Swal = require('sweetalert2')
+        Swal.fire({
+            position: 'top',
+            // icon: 'success',
+            title: isSuperAdmin ? 'Logged In as Super Admin' : isAdmin ? 'Logged In as Admin' : 'Logged In as User',
+            html: 'Name : ' + loggedInUser.name + '<br />' + 'Email : ' + loggedInUser.email,
+            // text: isSuperAdmin ? 'You are Super Admin' : isAdmin ? 'You are Admin' : 'You are User',
+            showConfirmButton: false,
+            timer: 2500,
+            timerProgressBar: true,
+            width: '500px',
+            background: '#FEE3EC',
+            color: '#FF1700',
         })
     }
     return (
@@ -59,16 +77,23 @@ const Navbar = () => {
                             <Link to="/dashboard" class="nav-link">DASHBOARD</Link>
 
                             <Link class="nav-link" to="/doctors">DOCTORS</Link>
-                            {/* <Link class="nav-link" to="/">EMERGENCY</Link>
-                            <Link class="nav-link" to="/">BLOOD BANK</Link>
-                            <Link class="nav-link" to="/">HEALTH TIPS</Link> */}
+
                             {
-                                loggedInUser.email ? loggedInUser.photo ? <Link className="nav-link"><img className='user-img' src={loggedInUser.photo} id={isSuperAdmin ? "super-admin" : isAdmin ? "admin" : ""} alt="" /></Link> : <Link className="nav-link" id="user-name">{loggedInUser.name}</Link> : <Link to="/login" className="nav-link btn btn-login px-2">LOG IN</Link>
+                                loggedInUser.email ? loggedInUser.photo ? <Link onClick={() => showNotificationForIdentification()} className="nav-link photo-link"><img className='user-img' src={loggedInUser.photo} id={isSuperAdmin ? "super-admin" : isAdmin ? "admin" : ""} alt="" /></Link> : <Link className="nav-link" id="user-name">{loggedInUser.name}</Link> : <Link to="/login" className="nav-link btn btn-login px-2">LOG IN</Link>
+
                             }
 
                             {
                                 loggedInUser.email ? <Link className="sign-out-btn btn-lg" onClick={() => { showNotification(); setLoggedInUser({}) }}>LOG OUT</Link> : ''
                             }
+
+                            {/* {loggedInUser && <div className='identification-show'>
+                                {
+                                    isSuperAdmin ? <p>You are Super Admin</p> : isAdmin ? <p>You are Admin</p> : <p>You are user</p>
+                                }
+                            </div>
+                            } */}
+
                         </div>
                     </div>
                 </div>
