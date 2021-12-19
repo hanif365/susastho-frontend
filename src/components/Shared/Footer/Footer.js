06c4ca171bf3167279e10d8c348159b2b807d46a
hanif365 <m.a.hanifkhan64@gmail.com>
2021-12-19 23:49:30 +0600

    footer design update

--- src/components/Shared/Footer/Footer.js
+++ src/components/Shared/Footer/Footer.js
@@ -6,20 +6,33 @@ import { faFacebook, faGithub, faInstagram, faLinkedin } from '@fortawesome/free
 
 const Footer = () => {
     return (
-        <div className="bg-colorful footer-main py-3 ">
-            <ul className="d-flex justify-content-center list-unstyled">
-                <a href="https://github.com/hanif365" target="_blank" className="fa-link me-3"><FontAwesomeIcon icon={faGithub} /></a>
-                <a href="https://www.linkedin.com/in/programmerhanif/" className="me-3 fa-link" target="_blank"><FontAwesomeIcon icon={faLinkedin} /></a>
-                <a href="https://www.facebook.com/M.A.HanifKhaan/" target="_blank" className="fa-link me-3"><FontAwesomeIcon icon={faFacebook} /></a>
-                <a href="https://www.instagram.com/abuhanif.cse3/" className="fa-link me-3" target="_blank"><FontAwesomeIcon icon={faInstagram} /></a>
-                <a href="https://hanif.netlify.app/" className="fa-link me-3" target="_blank"><FontAwesomeIcon icon={faGlobe} /></a>
-            </ul>
-            <div className=" d-flex justify-content-center  text-light">
-                @2021 <span className="project-name mx-2"> SUSASTHO </span> - All Rights reserved. Created By <span className="creator-name mx-2"> <a href="https://hanif.netlify.app/" target="_blank">M.A.HANIF</a> </span>
-            </div>
+        <div className="container-fluid">
+            <div className="row">
+                <div className="bg-colorful footer-main py-3 ">
+                    <ul className="d-flex justify-content-center list-unstyled">
+                        <a href="https://github.com/hanif365" target="_blank" className="fa-link me-3"><FontAwesomeIcon icon={faGithub} /></a>
+                        <a href="https://www.linkedin.com/in/programmerhanif/" className="me-3 fa-link" target="_blank"><FontAwesomeIcon icon={faLinkedin} /></a>
+                        <a href="https://www.facebook.com/M.A.HanifKhaan/" target="_blank" className="fa-link me-3"><FontAwesomeIcon icon={faFacebook} /></a>
+                        <a href="https://www.instagram.com/abuhanif.cse3/" className="fa-link me-3" target="_blank"><FontAwesomeIcon icon={faInstagram} /></a>
+                        <a href="https://hanif.netlify.app/" className="fa-link me-3" target="_blank"><FontAwesomeIcon icon={faGlobe} /></a>
+                    </ul>
+            
+                    <div className=" d-flex justify-content-center flex-wrap text-light">
+                        <div>
+                            @2021 <span className="project-name mx-2"> SUSASTHO </span> - All Rights reserved.
+                        </div>
+                        <div>
+                            Created By <span className="creator-name mx-2"> <a href="https://hanif.netlify.app/" target="_blank">M.A.HANIF</a> </span>
+                        </div>
 
-            <div>
-                <a href="#" className="bottom-to-top"><FontAwesomeIcon icon={faArrowUp} size="2x" /></a>
+                    </div>
+
+
+
+                    <div>
+                        <a href="#" className="bottom-to-top"><FontAwesomeIcon icon={faArrowUp} size="2x" /></a>
+                    </div>
+                </div>
             </div>
         </div>
     );
